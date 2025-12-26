0bff6e1b19d3574f23470a9e66239a7957cf2d9d
Harshit Goyal <harshitgoyal938@gmail.com>
2025-12-27 00:13:37 +0530

    Initial commit - added project files

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,21 @@
+
+
+// 1-Let,const and var ki kahani
+
+// const accountId =14453
+// let accountEmail="Harshit@gmail.com";
+// var accountPassword="12345";
+// accountCity="jaipur";
+// console.table([accountId,accountEmail,accountPassword,accountCity])
+// console.log(accountId);
+
+// 2-Datatypes and ECMA standards
+
+"use strict";
+// alert(3+3)(we are using nodejs not browser)
+
+let score=null
+console.log(typeof score);
+console.log(typeof(score))
+let valueInNumber=Number(score)
+console.log(valueInNumber)
